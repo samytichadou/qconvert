30c57992bf3eefaaba3320a60366cb3accecc22c
samytichadou <samytichadou@gmail.com>
2018-03-07 12:04:30 +0100

    take off json format
    clearer txt file

--- datas/QConvert_infos.bat
+++ datas/QConvert_infos.bat
@@ -5,6 +5,6 @@ setlocal ENABLEDELAYEDEXPANSION
 set fName=%1
 
 for /f "tokens=* delims= " %%F in ('echo %fName%') do (
-"%ffprobe%" -v quiet -print_format json -show_format -show_streams "%%~fF" > "%%~dpnF_infos.txt")
+"%ffprobe%" -v quiet -show_format -show_streams "%%~fF" > "%%~dpnF_infos.txt")
 
 END
